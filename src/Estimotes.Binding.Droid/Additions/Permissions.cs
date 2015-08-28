afa44bec20f7558c8e112d9b7ffccdd79b8c681d
Redth <jondick@gmail.com>
2015-08-28 15:24:55 -0400

    Made feature not required

--- src/Estimotes.Binding.Droid/Additions/Permissions.cs
+++ src/Estimotes.Binding.Droid/Additions/Permissions.cs
@@ -3,4 +3,4 @@ using Android.App;
 
 [assembly: UsesPermission(Manifest.Permission.Bluetooth)]
 [assembly: UsesPermission(Manifest.Permission.BluetoothAdmin)]
-[assembly: UsesFeature("android.hardware.bluetooth_le")]
+[assembly: UsesFeature("android.hardware.bluetooth_le", Required = false)]
